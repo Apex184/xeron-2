5f76ab3bb19b39f197a817919a5ccb9629aed83f
Akindele <akindeleapex@gmail.com>
2025-05-22 07:03:44 +0100

    commented out team members

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Header from './components/layout/Header';
-import Hero from './components/sections/Hero';
-import Features from './components/sections/Features';
-import WhyJoin from './components/sections/WhyJoin';
-import ProductVisuals from './components/sections/ProductVisuals';
-import TeamMembers from './components/sections/TeamMembers';
-import WaitlistForm from './components/sections/WaitlistForm';
-import ContactForm from './components/sections/ContactForm';
-import Footer from './components/layout/Footer';
-import { ThemeProvider } from './context/ThemeContext';
-import { LanguageProvider } from './context/LanguageContext';
-import './styles/animations.css';
+import React from "react";
+import Header from "./components/layout/Header";
+import Hero from "./components/sections/Hero";
+import Features from "./components/sections/Features";
+import WhyJoin from "./components/sections/WhyJoin";
+import ProductVisuals from "./components/sections/ProductVisuals";
+// import TeamMembers from "./components/sections/TeamMembers";
+import WaitlistForm from "./components/sections/WaitlistForm";
+import ContactForm from "./components/sections/ContactForm";
+import Footer from "./components/layout/Footer";
+import { ThemeProvider } from "./context/ThemeContext";
+import { LanguageProvider } from "./context/LanguageContext";
+import "./styles/animations.css";
 
 function App() {
   return (
@@ -23,7 +23,7 @@ function App() {
             <Features />
             <ProductVisuals />
             <WhyJoin />
-            <TeamMembers />
+            {/* <TeamMembers /> */}
             <WaitlistForm />
             <ContactForm />
           </main>
